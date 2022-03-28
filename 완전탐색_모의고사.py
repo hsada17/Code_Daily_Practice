e8e047d3d043c4e39df337d2e2158ba3797631f5
Sunah Han <97348375+hsada17@users.noreply.github.com>
2022-03-28 21:10:58 +0900

    Update 완전탐색_모의고사.py

--- 완전탐색_모의고사.py
+++ 완전탐색_모의고사.py
@@ -1,21 +1,6 @@
 
 # https://programmers.co.kr/learn/courses/30/lessons/42840
 
-# stu1 = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
-# stu2 = [2, 1, 2, 3, 2, 4, 2, 5, 2, 1]
-# stu3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-#
-# stuList = [stu1, stu2, stu3]
-#
-# answer = [1,2]
-# for stu in stuList:
-#     print(stu)
-#     answer = [stuList.index(stu) + 1]
-#     print(answer)
-
-
-
-
 def solution(answers):
     stu1 = [1,2,3,4,5]
     stu2 = [2,1,2,3,2,4,2,5]
@@ -38,5 +23,3 @@ def solution(answers):
             result.append(idx+1)
 
     return result
-
-print(solution([4,4,4,2,3,1,2,3,4,5,5,4,3,5,3,2]))
